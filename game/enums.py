14c80c9b3d25f2f854e3d50767b14cd27b6dc26d
Wlad5 <cebotarivlad814@gmail.com>
2026-02-23 11:00:37 +0100

    Number all the enum constants with values from 0

--- game/enums.py
+++ game/enums.py
@@ -1,30 +1,24 @@
 from enum import Enum
 class Direction(Enum):
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
-class Action(Enum):
-    TURN_LEFT = 1
-    TURN_RIGHT = 2
-    STRAIGHT = 3
-    MOVE_UP = 4
-    MOVE_DOWN = 5
 
 class GameStatus(Enum):
-    RUNNING = 1
-    PAUSED = 2
-    GAME_OVER = 3
-    WIN = 4
+    RUNNING = 0
+    PAUSED = 1
+    GAME_OVER = 2
+    WIN = 3
 
 class CellType(Enum):
     EMPTY = 0
     SNAKE = 1
-    FOOD = 3
-    WALL = 4
+    FOOD = 2
+    WALL = 3
 
 class EventType(Enum):
-    ATE_FOOD = 1
-    HIT_WALL = 2
-    HIT_SELF = 3
+    ATE_FOOD = 0
+    HIT_WALL = 1
+    HIT_SELF = 2
